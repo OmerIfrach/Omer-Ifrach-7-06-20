e57b15601beb5fe92579aec5c63147cec5063239
Omer Ifrach <omer.ifrach1@gmail.com>
2020-06-11 22:00:20 +0300

    Updates

---
+++ precache-manifest.30e2e9fcdfe34b5b9dfd1a3d8fee5216.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a23aaee38199b9af1a488a5514458dd9",
+    "url": "/Omer-Ifrach-7-06-20/index.html"
+  },
+  {
+    "revision": "e3fc28202ef8d30c470b",
+    "url": "/Omer-Ifrach-7-06-20/static/css/main.55bc5953.chunk.css"
+  },
+  {
+    "revision": "ef0013f31415f4bced5b",
+    "url": "/Omer-Ifrach-7-06-20/static/js/2.04f00c3c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Omer-Ifrach-7-06-20/static/js/2.04f00c3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3fc28202ef8d30c470b",
+    "url": "/Omer-Ifrach-7-06-20/static/js/main.5e9dace8.chunk.js"
+  },
+  {
+    "revision": "05858f4eae44185d4127",
+    "url": "/Omer-Ifrach-7-06-20/static/js/runtime-main.0cce99dc.js"
+  },
+  {
+    "revision": "a62b87e25f1d47a85ed613e132465af9",
+    "url": "/Omer-Ifrach-7-06-20/static/media/dark_mode_background.a62b87e2.jpg"
+  },
+  {
+    "revision": "8edc276514df9b5025d3f63f794cc6e3",
+    "url": "/Omer-Ifrach-7-06-20/static/media/light_mode_background.8edc2765.jpg"
+  },
+  {
+    "revision": "782c0c0f8d97ef03d5294850253b34ec",
+    "url": "/Omer-Ifrach-7-06-20/static/media/light_mode_icon.782c0c0f.png"
+  }
+]);
